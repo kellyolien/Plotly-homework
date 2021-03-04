644fe4f88db7ba290bf5c9f7f0bb48eda4e51e05
Kelly OlienC:/Users/Kelly/anaconda3/python.exe <kdubois77@hotmail.com>
2021-03-04 13:18:01 -0600

    ready to submit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -71,14 +71,14 @@ var layout = {
     Plotly.newPlot("bar", data, layout);
 
 }
-function greeting(name) {
-  // console.log("My name is", name)
-  var reverse = name.split("").reverse().join("")
-  console.log(reverse)
-}
+// function greeting(name) {
+//   // console.log("My name is", name)
+//   var reverse = name.split("").reverse().join("")
+//   console.log(reverse)
+// }
 
-greeting("Kelly")
-greeting("Philip")
+// greeting("Kelly")
+// greeting("Philip")
 
 function buildBubblePlot(filterSampleData){
 
@@ -111,129 +111,5 @@ var bubbleLayout = {
 Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 }
-//     d3.json('data/samples.json')
-//         .then (data=> {
-//         var filterSampleData = data.samples
-//         .filter(sample => sample.id == nameValue)
-//         console.log(filterSampleData)
-    
-
-//     var barTrace = {
-//         x: filterSampleData.sample_values.slice(0,10).reverse(),
-//         y: filterSampleData.otu_ids.slice(0,10).map(otuid => `OTU ${otuid}`).reverse(),
-//         text: filterSampleData.otu_labels.slice(0,10).reverse(),
-//         hoverlabel: {font: {size: 12}},
-//         marker: {
-//             color: 'blue',
-//             opacity: 1,
-//         },
-//         type: 'bar',
-//         orientation: 'h'
-        // }
-
-    // Grab values from the data json object to build the plots
-    // var sampleValues = data.samples[0].filterSampleData.slice(0,10).reverse();
-    // var sampleValues = filterSampleData[0].sample_values.slice(0,10).reverse();
-    // console.log(sampleValues)
-
-    // // // Labels for chart
-    // // var otuIDs = data.samples[0].otu_ids;
-    // var otuIDs = filterSampleData.otu_ids;
-    // console.log(otuIDs)
-
-     // Hovertext for chart
-    // var otuLabels = data.samples[0].otu_labels.slice(0,10);
-    // console.log(otuLabels)
-
-      // Top ten IDs
-    // var topID = (data.samples[0].otu_ids.slice(0,10)).reverse();
-
-    // // Make labels
-    // var labels = topID.map(d => "" + d);
-    // console.log(`OTU IDs: ${labels}`)
-    
-    // var trace1 = {
-    //     x: sampleValues,
-    //     y: otuIDs,
-    //     // text: otuLabels,
-    //     marker: {
-    //     color: 'blue'}, 
-    //     type:"bar",
-    //     orientation: "h",
-
-    //     };  
-    //     // Create the data array for our plot
-    // var data = [trace1];
-
-    // // Define our plot layout
-    // var layout = {
-    //     title: "Top 10 OTU",
-    //     yaxis: { tickmode: "linear" },
-    //     margin: {
-    //         l: 100,
-    //         r: 100,
-    //         t: 100,
-    //         b: 40
-    // }
-    // };
-    // // Plot the chart to a div tag with id "bar"
-    // Plotly.newPlot("bar", data, layout);
-
-    // };
 
   });
-
-
-
-
-    
-
-   
-
-  
-
-
-
-// // Bubble chart
-
-//     var trace2 = {
-//         x: otuIDs,
-//         y: sampleValues,
-//         text: otuLabels,
-//         mode: "markers",
-//         marker: {
-//             size: sampleValues,
-//             color: otuIDs, 
-//             colorscale: "Earth"},
-//         type: "scatter"
-
-//         };
-
-//     var data2 = [trace2];
-
-//     var layoutb = {
-//         xaxis: {title: "OTU ID"},
-//         height: 600,
-//         width: 1000
-//     };
-
-//     Plotly.newPlot("bubble", data2, layoutb);
-
-    
-
-// function Meta(sample) {
-//     //select the Panel with id #sample-metadata
-//     d3.json(`/metadata/${sample}`).then((data) => {
-//         var Panel = d3.select("#sample-metadata");
-//         // clear any existing metadata
-//         Panel.html("");
-//         //use `Object.entries` to add each key and value pair to panel
-//         //inside the loop use d3 to append new tags for each key-value in metadata
-//         Object.entries(data).forEach(([key,value]) => {
-//             Panel.append("h6").text(`${key}:${value}`);
-//         });
-//     });
-
-// // function init() {
-// //     var selector = d3.select("selDataset")};
-// )};
